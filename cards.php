6a34e2f40dc8553f1e78190188a783b936eed06f
ricardojve12 <ricardojve12@gmail.com>
2023-12-09 20:43:45 -0600

    first commit

---
+++ cards.php
@@ -0,0 +1,8 @@
+<?php require_once "vistas/parte_superior.php" ?>
+
+<!---INICIO DEL CONTENIDO PRINCIPAL-->
+<div class="container">
+    <h1>contenido principal</h1>
+</div>
+<!--fin del contenido princila-->
+<?php require_once "vistas/parte_inferior.php" ?>
